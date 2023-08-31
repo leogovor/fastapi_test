093175be409c3582b753e577d5cdc14653748bc0
leohow <leogovor@gmail.com>
2023-08-31 12:40:31 +0200

    afstapi test

---
+++ main.py
@@ -0,0 +1,18 @@
+from typing import Union
+
+from fastapi import FastAPI
+import json
+from datetime import datetime
+app = FastAPI()
+
+
+@app.get("/")
+
+def read_root():
+    return {"Hello": "World", "time": datetime.now().strftime('%d/%h/%y')}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
