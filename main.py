ef9252ad6de87e150e320dc407e6650c1aaf4b61
leohow <leogovor@gmail.com>
2023-08-31 12:50:55 +0200

    afstapi test

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#need 2 get FastAPI: pip install fastapi, and ASGI server pip install uvicorn
 from typing import Union
 
 from fastapi import FastAPI
@@ -5,12 +6,10 @@ import json
 from datetime import datetime
 app = FastAPI()
 
-
 @app.get("/")
 
 def read_root():
     return {"Hello": "World", "time": datetime.now().strftime('%d/%h/%y')}
-
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
