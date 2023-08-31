78da0ad02f59bc136bff9759a6423112eed91ad3
leohow <leogovor@gmail.com>
2023-08-31 13:09:39 +0200

    afstapi test

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 #need 2 get FastAPI: pip install fastapi, and ASGI server pip install uvicorn
+import datetime
 from typing import Union
-
+import requests
 from fastapi import FastAPI
-import json
-from datetime import datetime
+
 app = FastAPI()
+ip = requests.get('https://api.ipify.org').content.decode('utf8')
+print('My public IP address is: {}'.format(ip))
 
 @app.get("/")
 
 def read_root():
-    return {"Hello": "World", "time": datetime.now().strftime('%d/%h/%y')}
-
+    return {"Hello": "Researcher!", "time": datetime.datetime.now().strftime('%d/%h/%y'), "your public ip is:":(ip)}
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
